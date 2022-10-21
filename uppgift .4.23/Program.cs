1058da2791ba1454d6fa360a631529178c203946
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-21 14:17:21 +0200

    Add project files.

---
+++ uppgift .4.23/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace uppgift_4._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+
+            int tal = int.Parse(Console.ReadLine());
+
+            int störst = tal;
+
+            string svar = "";
+            while (svar != "n")
+            {
+                Console.WriteLine("Vill du skriva in ett till heltal? (j/n)");
+                svar = Console.ReadLine();
+
+                if (svar == "n")
+                {
+                    break;
+                }
+                Console.WriteLine("Skriv in ett heltal");
+                int nyttTal = int.Parse(Console.ReadLine());
+
+                if (nyttTal > störst)
+                {
+                    störst = nyttTal;
+                }
+
+            }
+
+            Console.WriteLine($"Det största talet du skrev var {störst}");
+        }
+    }
+}
